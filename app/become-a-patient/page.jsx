6edd94c8a489687791c833d2e837ab5c8aaedc72
Rapheal Ogundiran <raphealobairekanmi@gmail.com>
2025-01-04 13:58:06 +0100

    Page SEO

--- app/become-a-patient/page.jsx
+++ app/become-a-patient/page.jsx
@@ -1,25 +1,15 @@
-import { NextSeo } from 'next-seo';
-import SEO from '../../config/seo.config';
 import FooterTop from "../../components/FooterTop/FooterTop";
-import PatientsLayout from "../../components/PatientsComp/PatientsLayout";
+import PatientsLayout from "../../components/PatientsComp/PatientsLayout"
 
 const Patients = () => {
-    // Ensure that you override specific SEO fields for this page.
-    const pageSEO = {
-        ...SEO,  // Spread the default SEO settings from seo.config.js
-        title: 'Become a Patient at Danicare Psychiatry | Start Your Mental Health Journey',  // Custom page title
-        description: 'Join Danicare Psychiatry and begin your personalized psychiatric treatment. Our team of professionals provides compassionate mental health care, including evaluations and therapy. Register today to start your journey to better mental health.', // Custom page description
-        
-    };
-
     return (
         <>
-            <NextSeo {...pageSEO} />
-            <PatientsLayout />
-            <FooterTop />
+        <PatientsLayout/>
+        <FooterTop/>
+        
         </>
     );
+
 };
 
 export default Patients;
-
