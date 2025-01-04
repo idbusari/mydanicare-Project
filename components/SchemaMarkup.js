27ef7040c3ba5b639ae47470e23d0372c3b683a8
Rapheal Ogundiran <raphealobairekanmi@gmail.com>
2025-01-04 15:20:19 +0100

    Schema Integration

--- components/SchemaMarkup.js
+++ components/SchemaMarkup.js
@@ -1,22 +1,48 @@
 // components/SchemaMarkup.js
-import { JsonLd } from 'next-schema-org';
+import { NextSeo } from 'next-seo';
 
 const SchemaMarkup = () => (
-  <JsonLd
-    item={{
-      '@context': 'https://schema.org',
-      '@type': 'LocalBusiness',
-      name: 'DaniCare Psychiatry',
-      address: {
-        '@type': 'PostalAddress',
-        streetAddress: 'Laredo Medical Center 1700 E Saunders Street, STE B475',
-        addressLocality: 'Laredo',
-        addressRegion: 'Texas',
-        postalCode: '78041',
-        addressCountry: 'US',
-      },
+  <NextSeo
+    title="DaniCare Psychiatry"
+    description="DaniCare Psychiatry provides quality mental health services in Laredo, Texas."
+    openGraph={{
       url: 'https://mydanicare.com',
+      title: 'DaniCare Psychiatry',
+      description: 'DaniCare Psychiatry provides quality mental health services in Laredo, Texas.',
+      images: [
+        {
+          url: 'https://mydanicare.com/images/clinic.jpg', // Add an image URL if you have one
+          width: 800,
+          height: 600,
+          alt: 'DaniCare Psychiatry Clinic',
+        },
+      ],
+      site_name: 'DaniCare Psychiatry',
     }}
+    twitter={{
+      handle: '@DaniCarePsych',
+      site: '@DaniCarePsych',
+      cardType: 'summary_large_image',
+    }}
+    additionalMetaTags={[
+      {
+        property: 'schema.org',
+        content: JSON.stringify({
+          '@context': 'https://schema.org',
+          '@type': 'LocalBusiness',
+          name: 'DaniCare Psychiatry',
+          address: {
+            '@type': 'PostalAddress',
+            streetAddress: 'Laredo Medical Center 1700 E Saunders Street, STE B475',
+            addressLocality: 'Laredo',
+            addressRegion: 'Texas',
+            postalCode: '78041',
+            addressCountry: 'US',
+          },
+          url: 'https://mydanicare.com',
+        }),
+      },
+    ]}
   />
 );
 
