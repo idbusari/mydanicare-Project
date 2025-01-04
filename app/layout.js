5394986e133940feb342048a5343ed93e7a91af1
Rapheal Ogundiran <raphealobairekanmi@gmail.com>
2025-01-04 09:50:05 +0100

    Analytics Configuration File

--- app/layout.js
+++ app/layout.js
@@ -31,9 +31,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>{metadata.title || "Default Title"}</title>
-        <meta name="description" content={metadata.description || "Default description"} />
-
+        
         {/* Adding Swiper CDN links */}
         <link
           rel="stylesheet"
