73ee73c9b13d290ba633c66331bf8beca52892ca
Ralphmore <raphealobairekanmi@gmail.com>
2025-01-23 19:18:02 +0100

    Test

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -14,7 +14,6 @@ export const Footer = () => {
                 layout="intrinsic"
                 width={159}
                 height={32}
-                
                 alt="DaniCare Logo" 
                 style={{color:"transparent"}}
               />
@@ -36,28 +35,36 @@ export const Footer = () => {
               <li><Link href="/psychiatry-treatment">Treatment</Link></li>
               <li><Link href="/psychiatrist">Our psychiatrist</Link></li>
               <li><Link href="/become-a-patient">Patients</Link></li>
-              <li><Link href="/become-a-patient"></Link></li>
             </ul>
             <p>
-<ol className="list-unstyled">
-<span>  </span>
-<p><span className="intro">Other Locations </span> <br/> Irving  <br/> Atlanta <br/>  </p>     
-</ol>
-
+              <span className="intro">Other Locations:</span> <br/> Irving <br/> Atlanta <br/>
             </p>
+
+            <div className="col-6 col-md-3 col-lg-2 mb-4">
+            <h5 className="mb-3">Our Solutions</h5>
+            <ul className="list-unstyled">
+              <li><Link href="/we-accept-insurance">Insurance</Link></li>
+              <li><Link href="/psychiatry-treatment">Treatment</Link></li>
+              <li><Link href="/psychiatrist">Our psychiatrist</Link></li>
+              <li><Link href="/become-a-patient">Patients</Link></li>
+            </ul>
+            <p>
+              <span className="intro">Other Locations:</span> <br/> Irving <br/> Atlanta <br/>
+            </p>
+          </div>
           </div>
     
           <div className="col-6 col-md-3 col-lg-2 mb-4">
             <h5 className="mb-3">Resources</h5>
             <ul className="list-unstyled">
-              <li><Link href="/our-faqs">Faqs</Link></li>
+              <li><Link href="/our-faqs">FAQs</Link></li>
               <li><Link href="/psychiatry-treatment">Anxiety Test</Link></li>
               <li><Link href="/psychiatry-treatment">Depression Test</Link></li>
               <li><Link href="/psychiatry-treatment">ADHD Test</Link></li>
-              <li><Link href="/become-a-patient"></Link></li>
-              <li><Link href="/become-a-patient"></Link></li>
             </ul>
-            <p><span className="intro"> </span> <br/> New Jersey <br/> California <br/> </p>    
+            <p>
+              <span className="intro">States We Serve:</span> <br/> New Jersey <br/> California <br/>
+            </p>
           </div>
     
           <div className="col-6 col-md-3 col-lg-2 mb-4">
@@ -66,74 +73,69 @@ export const Footer = () => {
               <li><Link href="/psychiatry-service-provider">About Us</Link></li>
               <li><Link href="/become-a-patient">Ask for Help</Link></li>
               <li><Link href="/psychiatry-care-registration">Become a Patient</Link></li>
-              <li><Link href="/become-a-patient"></Link></li>
-              <li><Link href="/become-a-patient"></Link></li>
-              <li><Link href="/become-a-patient"></Link></li>
             </ul> 
           </div>
     
           <div className="col-6 col-md-3 col-lg-3 mb-4">
             <h5 className="mb-3">Connect</h5>
             <ul className="list-unstyled">
-              <li><Link href="mailto: hello@mydanicare.com" style={{ color: "#ffcc04" }}>hello@mydanicare.com</Link></li>
+              <li><Link href="mailto:hello@mydanicare.com" style={{ color: "#ffcc04" }}>hello@mydanicare.com</Link></li>
               <li><Link href="#">Terms of Use</Link></li>
               <li><Link href="/privacy-policy">Privacy</Link></li>
             </ul>
           </div>
+          
           <div className="">
-    <div className="d-flex text-center gap-3">
-      <Link href="https://www.instagram.com/danicarepsychiatry/" target="_blank" aria-label="Instagram">
-        <Image
-          src="/images/instagram.svg"
-          alt="Instagram logo"
-          width={24}
-          height={24}
-          priority={false}
-        />
-      </Link>
-      <Link href="https://www.facebook.com/danicarepsychiatry" target="_blank" aria-label="Facebook">
-        <Image
-          src="/images/facebook.svg"
-          alt="Facebook logo"
-          width={24}
-          height={24}
-          priority={false}
-        />
-      </Link>
-      <Link href="#" target="_blank" aria-label="Twitter">
-        <Image
-          src="/images/twitter.svg"
-          alt="Twitter logo"
-          width={24}
-          height={24}
-          priority={false}
-        />
-      </Link>
-      <Link href="#" target="_blank" aria-label="LinkedIn">
-        <Image
-          src="/images/linkedin.svg"
-          alt="LinkedIn logo"
-          width={24}
-          height={24}
-          priority={false}
-        />
-      </Link>
-      <Link href="#" target="_blank" aria-label="YouTube">
-        <Image
-          src="/images/youtube.svg"
-          alt="YouTube logo"
-          width={24}
-          height={24}
-          priority={false}
-        />
-      </Link>
-    </div>
-    </div>
-        
+            <div className="d-flex text-center gap-3">
+              <Link href="https://www.instagram.com/danicarepsychiatry/" target="_blank" aria-label="Instagram">
+                <Image
+                  src="/images/instagram.svg"
+                  alt="Instagram logo"
+                  width={24}
+                  height={24}
+                  priority={false}
+                />
+              </Link>
+              <Link href="https://www.facebook.com/danicarepsychiatry" target="_blank" aria-label="Facebook">
+                <Image
+                  src="/images/facebook.svg"
+                  alt="Facebook logo"
+                  width={24}
+                  height={24}
+                  priority={false}
+                />
+              </Link>
+              <Link href="#" target="_blank" aria-label="Twitter">
+                <Image
+                  src="/images/twitter.svg"
+                  alt="Twitter logo"
+                  width={24}
+                  height={24}
+                  priority={false}
+                />
+              </Link>
+              <Link href="#" target="_blank" aria-label="LinkedIn">
+                <Image
+                  src="/images/linkedin.svg"
+                  alt="LinkedIn logo"
+                  width={24}
+                  height={24}
+                  priority={false}
+                />
+              </Link>
+              <Link href="#" target="_blank" aria-label="YouTube">
+                <Image
+                  src="/images/youtube.svg"
+                  alt="YouTube logo"
+                  width={24}
+                  height={24}
+                  priority={false}
+                />
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
-    
-    
     );
-    }
+};
