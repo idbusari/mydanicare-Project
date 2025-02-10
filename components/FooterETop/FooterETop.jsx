a7f25b300f0bbcaa9b822189a8fde1852a4f4223
Ralphmore <raphealobairekanmi@gmail.com>
2025-02-10 21:54:07 +0100

    Footer

--- components/FooterETop/FooterETop.jsx
+++ components/FooterETop/FooterETop.jsx
@@ -1,44 +1,31 @@
-import "./FooterETop.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebookF, faTwitter, faInstagram, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const FooterETop = () => {
   return (
-    <div className="container-fluid footerETop">
-      <div className="row text-white py-4">
-        {/* Four text columns */}
-        <div className="col-md-3 col-sm-6 footer-column">
-          <h4>About Us</h4>
-          <p>We are dedicated to providing quality mental health care and support services to our community.</p>
-        </div>
-        <div className="col-md-3 col-sm-6 footer-column">
-          <h4>Services</h4>
-          <p>We offer therapy, counseling, and psychiatric evaluations tailored to your needs.</p>
-        </div>
-        <div className="col-md-3 col-sm-6 footer-column">
-          <h4>Contact</h4>
-          <p>Email: contact@mydanicare.com<br />Phone: +1 (555) 123-4567</p>
-        </div>
-        <div className="col-md-3 col-sm-6 footer-column">
-          <h4>Resources</h4>
-          <p>Explore our articles, guides, and self-help tools to improve your mental well-being.</p>
-        </div>
+    <div className="container-fluid footerTop">
+      <div className="row">
+        {/* 4 Columns with Text */}
+        <div className="col-md-3"><h4>About Us</h4><p>We provide the best services.</p></div>
+        <div className="col-md-3"><h4>Contact</h4><p>info@example.com</p></div>
+        <div className="col-md-3"><h4>Support</h4><p>Help Center & FAQs</p></div>
+        <div className="col-md-3"><h4>Newsletter</h4><p>Subscribe for updates.</p></div>
       </div>
-
-      {/* Social Media Row */}
-      <div className="row justify-content-center">
-        <div className="col-12 text-center py-3 social-icons">
+      
+      {/* Social Media Icons in Centered Row */}
+      <div className="row justify-content-center mt-4">
+        <div className="col-auto">
           <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faFacebookF} />
+            <FontAwesomeIcon icon={faFacebook} size="2x" className="social-icon" />
           </a>
           <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faTwitter} />
+            <FontAwesomeIcon icon={faTwitter} size="2x" className="social-icon" />
           </a>
           <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faInstagram} />
+            <FontAwesomeIcon icon={faInstagram} size="2x" className="social-icon" />
           </a>
           <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faLinkedinIn} />
+            <FontAwesomeIcon icon={faLinkedin} size="2x" className="social-icon" />
           </a>
         </div>
       </div>
