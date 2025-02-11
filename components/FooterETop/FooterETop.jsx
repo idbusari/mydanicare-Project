9f13cc81c0d5b8b0b4a2b25e15d1c17e32ab4864
Ralphmore <raphealobairekanmi@gmail.com>
2025-02-11 15:27:09 +0100

    LOcation Sidebar and Footer

--- components/FooterETop/FooterETop.jsx
+++ components/FooterETop/FooterETop.jsx
@@ -1,5 +1,4 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 import "./FooterETop.scss";
 
 const FooterETop = () => {
