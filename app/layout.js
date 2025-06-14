45ab358849045ba5334f3699357c883bd2a153c3
Ralphmore <raphealobairekanmi@gmail.com>
2025-06-14 18:53:51 +0100

    GTM Integration

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Outfit, DM_Sans } from 'next/font/google';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
 import "./globals.css";
 import { Header } from '../components/Header/Header';
 import { Footer } from '../components/Footer/Footer';
@@ -10,15 +9,12 @@ import { FooterBottom } from '../components/FooterBottom/FooterBottom';
 import HeaderTop from '../components/HeaderTop/HeaderTop';
 import { GoogleAnalytics } from '@next/third-parties/google';
 import seoConfig from '../config/seo.config';
-import TagManager from 'react-gtm-module'; // Import TagManager
-
-
+import Script from 'next/script'; // Import Next.js Script component
 
 // Import FooterETop
 import FooterETop from '../components/FooterETop/FooterETop';
-import SocialMediaSidebar from "../components/SocialMediaSidebar/SocialMediaSidebar"; // Import sidebar component
-import SocialMediaRow from '../components/SocialMediaRow/SocialMediaRow'; // Import social media footer
-
+import SocialMediaSidebar from "../components/SocialMediaSidebar/SocialMediaSidebar";
+import SocialMediaRow from '../components/SocialMediaRow/SocialMediaRow';
 
 const outfit = Outfit({
   subsets: ['latin'],
@@ -38,6 +34,8 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const GTM_ID = "GTM-PN7M6L9W"; // Your GTM container ID
+
   return (
     <html lang="en">
       <head>
@@ -53,18 +51,38 @@ export default function RootLayout({ children }) {
 
         <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
         <script src="https://unpkg.com/swiper/swiper-bundle.min.js" defer></script>
+        
+        {/* Google Tag Manager Script */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+        
         <GoogleAnalytics gaId="G-BVBHZ1NWSN" />
       </head>
       <body className={`${outfit.variable} ${dmSans.variable}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
       
-      <HeaderTop />
-      <SocialMediaSidebar />
-      <Header />
+        <HeaderTop />
+        <SocialMediaSidebar />
+        <Header />
         <ClientLayout>
           {children}
         </ClientLayout>
         <Footer />
-        {/* FooterETop now appears here, after Footer but before FooterBottom */}
         <FooterETop />
         <SocialMediaRow />
         <FooterBottom />
